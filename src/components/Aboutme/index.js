e628f1afab53e090064cde7e095d691ccf59bb62
nisha4r <nisha.4r@gmail.com>
2023-08-06 17:58:04 -0600

    Aboutme

---
+++ src/components/Aboutme/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function Aboutme() {
+    return (
+        <section>
+            <h1>About Me</h1>
+
+            <div>
+                <p>Hello, I'm Nishanthi, 33 year old living at Herriman, Utah. I am a Web Developer learning Web development from Coding Bootcamp at UofU 2023 Feb Batch. I'm fueled with a passion for understanding requirements and the ability convert requirements into Web products. I am a "forever student," eager to build on my academic foundations in Web development and IT.</p>
+
+                <p>I hunger for knowledge and am determined to learn and create the best solution for web development.</p>
+
+                <p>I believe mindfulness in the workplace is crucial to success—a tenet. I live out my self-interests in yoga, meditation, gardening, and painting. I'm currently working as a homemaker and am always interested in challenges. Reach out to nisha.4r@gmail.com to connect!</p>
+            </div>
+        </section>
+    );
+}
+
+export default Aboutme;
