003f87f2d22de2376e5e5b9c2d79aa83fa3cb2f9
nisha4r <nisha.4r@gmail.com>
2023-08-06 18:36:15 -0600

    Add NavBar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function NavBar(props){
+
+    return(
+        <nav>
+            <ul>
+                <li><a href="#"> About me</a></li>
+                <li><a href="#"> Portfolio</a></li>
+                <li><a href="#"> Contact</a></li>
+                <li><a href="#"> Resume</a></li>
+              
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
