cb1ebbdf858eb7428352faa5dbc5040dea4fd680
nisha4r <nisha.4r@gmail.com>
2023-08-06 18:15:50 -0600

    Adding App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Aboutme from './components/Aboutme';
+import Profile from './components/Profile';
 
 
 
@@ -6,8 +10,11 @@ function App() {
   return (
     <>
       <title>Nishanthi's Portfolio </title>
-      <main><div className="portfolio-app"></div>
+      <Header></Header>
+      <main>
+        <div className="portfolio-app"> Portfolio here</div>
       </main>
+      <Footer></Footer>
     </>
   );
 }
