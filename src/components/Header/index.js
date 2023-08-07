bc5938e1db7ef761242d99d715659172a264de6c
nisha4r <nisha.4r@gmail.com>
2023-08-06 18:32:53 -0600

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import NavBar from '../NavBar'
+
+function Header(props) {
+    return (<div>
+        <section>
+            <header>
+                <div>
+                    <h1>Nishanthi's Portfolio</h1>
+                </div>
+                <div>
+                    <NavBar></NavBar>
+                </div>
+            </header>
+        </section>
+        <section>
+            <div>
+                <h3>Welcome to Nishanthi's Portfolio</h3>
+                <p>This portfolio is build with React components. List of projects are listed below, that she developed in this course.</p>
+            </div>
+        </section>
+    </div>);
+
+}
+
+export default Header;
