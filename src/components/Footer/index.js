2f9087a6be7e96f7d8950de4224ff4689867f9fa
nisha4r <nisha.4r@gmail.com>
2023-08-06 18:21:04 -0600

    Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+function footer(){
+    return (
+        <footer>
+            <h3>Created by Nishanthi©</h3>
+            <p>
+              <ul>
+                <li>GitHub</li>
+                <li>LinkedIn</li>
+                <li>Stackoverflow</li>
+              </ul>
+            </p>
+        </footer>
+    );
+}
+export default footer;
