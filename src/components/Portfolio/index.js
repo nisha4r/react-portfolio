1bdf03ac5e2eb16325e4b01e0a3f19fa0f90f6d6
nisha4r <nisha.4r@gmail.com>
2023-08-06 19:05:24 -0600

    Portfolio and Profile

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+        <div>
+            <section>
+                <div>
+                    <h2>Work</h2>
+                </div>
+                <div>
+                    <div>
+                        <a href="#"> Image</a>
+                    </div>
+                    <div>
+                        <h3>Project Name</h3>
+                        <p>Project Description</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default Portfolio;
---
+++ src/components/Profile/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Profile(){
+    return(<div>
+        <section>
+            <div>
+                <h1>Resume</h1>
+            </div>
+            <div>
+                <h3> Download my resume</h3>
+            </div>
+        </section>
+        <section>
+            <div>
+                <div>
+                    <p>Front End</p>
+                    <p>
+                        <ul>
+                            <li>HTML5</li>
+                            <li>CSS</li>
+                            <li>Javascript</li>
+                            <li>Advanced CSS</li>
+                            <li>Bootstrap</li>
+                            
+                        </ul>
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>);
+}
+
+export default Profile;
