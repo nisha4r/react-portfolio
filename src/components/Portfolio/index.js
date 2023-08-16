5f9a9b38d8778e89b5e9c4a5cea7d026a5fb7e4e
nisha4r <nisha.4r@gmail.com>
2023-08-15 23:15:07 -0600

    Adding food lette

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,104 +1,104 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import { HStack, Center, CardBody, Badge, Button, SlideFade, Fade, ScaleFade } from '@chakra-ui/react';
-import htmlImage from "../../assets/html_css.png";
+import homeroom from "../../assets/homeroom.png";
 import foodImage from "../../assets/food.avif";
 import byeByeFresh from "../../assets/bbfreshlogo.png";
 import { Image } from '@chakra-ui/react'
-import {useInViewport} from 'react-in-viewport'
+import { useInViewport } from 'react-in-viewport'
 import { SimpleGrid, GridItem, Heading, Divider, Box, Text, Container, Stack, Card } from '@chakra-ui/react';
-function Portfolio({color}) {
+function Portfolio({ color }) {
     const ref = useRef(null);
-     const {inViewport} = useInViewport(ref, {rootMargin: "-200px"},{disconnectOnLeave: false},{});
+    const { inViewport } = useInViewport(ref, { rootMargin: "-200px" }, { disconnectOnLeave: false }, {});
     return (
         <>
-            
-            
-            <Container maxW={'3x1'}  bgGradient="linear(to-t, green.100, teal.500)" p={5}>
+
+
+            <Container maxW={'3x1'} bgGradient="linear(to-t, green.100, teal.500)" p={5}>
                 <Stack as={Box} textAlign={'center'} spacing={{ base: 8, md: 14 }} pb={{ base: 18, md: 34 }}>
                     <Stack px={4} align={'center'} direction="row">
                         <HStack mx={5}>
                             <Heading color={'${color}.400'} fontWeight={800}>Project Work</Heading>
                         </HStack>
-                        
+
                     </Stack>
                     <Divider orientation='horizontal' />
                     <Stack spacing={4} px={4}>
-                       {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
-                            <Card overflow={'hidden'} direction={{base: "column"}} shadow={'md'} m={3} p={3} >
-                                <Image objectFit={'cover'} src={foodImage} alt='HTML CSS'></Image>
-                                <Stack>
-                                    <CardBody align="left">
-                                        <Heading> Project 1 - Food Lette</Heading>
-                                        <Text>Description</Text>
-                                        <HStack py={2}>
-                                            <Button>Link</Button>
-                                        </HStack>
-                                        <HStack spacing={2} pt={4}>
-                                            <Badge>Badge</Badge>
-                                        </HStack>
-                                    </CardBody>
-                                </Stack>
-                            </Card>
-                            {/* </Fade> */}
+                        {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
+                        <Card flexShrink={0} borderRadius='15px' overflow={'hidden'} direction={{ base: "column" }} shadow={'md'} m={3} p={3} width={{ base: 600, md: 800 }} bg={'gray.400'}>
+                            <Image objectFit={'cover'} src={foodImage} alt='HTML CSS' width={{ base: 600, md: 800 }} height={'400px'}></Image>
+                            <Stack>
+                                <CardBody align="left">
+                                    <Heading> Project 1 - Food Lette</Heading>
+                                    <Text>Most of us have been in a similar situation, that horrible time of day when you have to figure out what to eat, and make a decision. No one likes doing it, and so we have come together to try and solve this problem. Java Chips Food Generator is here to help you. This Food Generator takes out all of the complication and helps make your life easier. When you put in your address information, it will ask you three simple questions. Cuisine preference, minimum rating, and acceptiable distance. Based off of that information the generator will create a random order for you and deliver it to you. So you get to try new foods and not have to make a decision.</Text>
+                                    <HStack py={2}>
+                                        <Button>Link</Button>
+                                    </HStack>
+                                    <HStack spacing={2} pt={4}>
+                                        <Badge>Badge</Badge>
+                                    </HStack>
+                                </CardBody>
+                            </Stack>
+                        </Card>
+                        {/* </Fade> */}
                     </Stack>
                     <Stack spacing={4} px={4}>
-                       {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
-                            <Card overflow={'hidden'} direction={{base: "column"}} shadow={'md'} m={3} p={3}>
-                                <Image objectFit={'cover'} src={byeByeFresh} alt='HTML CSS'></Image>
-                                <Stack>
-                                    <CardBody align="left">
-                                        <Heading> Project 2 - Bye Bye Fresh</Heading>
-                                        <Text>Description</Text>
-                                        <HStack py={2}>
-                                            <Button>Link</Button>
-                                        </HStack>
-                                        <HStack spacing={2} pt={4}>
-                                            <Badge>Badge</Badge>
-                                        </HStack>
-                                    </CardBody>
-                                </Stack>
-                            </Card>
-                            {/* </Fade> */}
+                        {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
+                        <Card borderRadius='15px' overflow={'hidden'} direction={{ base: "column" }} shadow={'md'} m={3} p={3} width={{ base: 600, md: 800 }} bg={'gray.400'}>
+                            <Image objectFit={'cover'} src={byeByeFresh} alt='HTML CSS' width={{ base: 600, md: 800 }} height={'400px'}></Image>
+                            <Stack>
+                                <CardBody align="left">
+                                    <Heading> Project 2 - Bye Bye Fresh</Heading>
+                                    <Text>Description</Text>
+                                    <HStack py={2}>
+                                        <Button>Link</Button>
+                                    </HStack>
+                                    <HStack spacing={2} pt={4}>
+                                        <Badge>Badge</Badge>
+                                    </HStack>
+                                </CardBody>
+                            </Stack>
+                        </Card>
+                        {/* </Fade> */}
                     </Stack>
                     <Stack spacing={4} px={4}>
-                       {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
-                            <Card overflow={'hidden'} direction={{base: "column"}} shadow={'md'} m={3} p={3}>
-                                <Image objectFit={'cover'} src={htmlImage} alt='HTML CSS'></Image>
-                                <Stack>
-                                    <CardBody align="left">
-                                        <Heading> Project 3 - Homeroom</Heading>
-                                        <Text>Description</Text>
-                                        <HStack py={2}>
-                                            <Button>Link</Button>
-                                        </HStack>
-                                        <HStack spacing={2} pt={4}>
-                                            <Badge>Badge</Badge>
-                                        </HStack>
-                                    </CardBody>
-                                </Stack>
-                            </Card>
-                            {/* </Fade> */}
+                        {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
+                        <Card borderRadius='15px' overflow={'hidden'} direction={{ base: "column" }} shadow={'md'} m={3} p={3} width={{ base: 600, md: 800 }} bg={'gray.400'}>
+                            <Image objectFit={'cover'} src={homeroom} alt='HTML CSS' width={{ base: 600, md: 800 }} height={'400px'}></Image>
+                            <Stack>
+                                <CardBody align="left">
+                                    <Heading> Project 3 - Homeroom</Heading>
+                                    <Text>Description</Text>
+                                    <HStack py={2}>
+                                        <Button>Link</Button>
+                                    </HStack>
+                                    <HStack spacing={2} pt={4}>
+                                        <Badge>Badge</Badge>
+                                    </HStack>
+                                </CardBody>
+                            </Stack>
+                        </Card>
+                        {/* </Fade> */}
                     </Stack>
                     <Stack spacing={4} px={4}>
-                       {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
-                            <Card overflow={'hidden'} direction={{base: "column"}} shadow={'md'} m={3} p={3}>
-                                <Image objectFit={'cover'} src={htmlImage} alt='HTML CSS'></Image>
-                                <Stack>
-                                    <CardBody align="left">
-                                        <Heading> HTML & CSS</Heading>
-                                        <Text>Description</Text>
-                                        <HStack py={2}>
-                                            <Button>Link</Button>
-                                        </HStack>
-                                        <HStack spacing={2} pt={4}>
-                                            <Badge>Badge</Badge>
-                                        </HStack>
-                                    </CardBody>
-                                </Stack>
-                            </Card>
-                            {/* </Fade> */}
+                        {/* <Fade initialScale={0.5} in={inViewport > 0}  whileHover={{scale:0.8}} > */}
+                        <Card borderRadius='15px' overflow={'hidden'} direction={{ base: "column" }} shadow={'md'} m={3} p={3} width={{ base: 600, md: 800 }} bg={'gray.400'} >
+                            <Image objectFit={'cover'} src={homeroom} alt='HTML CSS' width={{ base: 600, md: 800 }} height={'400px'}></Image>
+                            <Stack>
+                                <CardBody align="left">
+                                    <Heading> HTML & CSS</Heading>
+                                    <Text>Description</Text>
+                                    <HStack py={2}>
+                                        <Button>Link</Button>
+                                    </HStack>
+                                    <HStack spacing={2} pt={4}>
+                                        <Badge>Badge</Badge>
+                                    </HStack>
+                                </CardBody>
+                            </Stack>
+                        </Card>
+                        {/* </Fade> */}
                     </Stack>
-               
+
                 </Stack>
             </Container>
         </>
