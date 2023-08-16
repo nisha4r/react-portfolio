1bd2c8495ee15e45bd13b34295b11c8249dcecfc
nisha4r <nisha.4r@gmail.com>
2023-08-16 00:06:20 -0600

    clean up

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Container, Box, Divider, Heading, Spacer } from '@chakra-ui/react';
+import { Container, Box, Tooltip, Heading } from '@chakra-ui/react';
 import { DownloadIcon } from '@chakra-ui/icons'
 function Profile() {
     const onButtonClick = () => {
@@ -19,7 +19,7 @@ function Profile() {
     return (<Container maxW={'3x1'} bgGradient="linear(to-t, green.100, teal.500)" p={5}>
         <Box>
 
-            <Heading> Download My Resume <DownloadIcon w={8} h={8} color="blue.500" onClick={onButtonClick}/> </Heading>
+            <Heading> Download My Resume  <DownloadIcon w={8} h={8} color="blue.500" onClick={onButtonClick} /></Heading>
         </Box>
     </Container>);
 }
